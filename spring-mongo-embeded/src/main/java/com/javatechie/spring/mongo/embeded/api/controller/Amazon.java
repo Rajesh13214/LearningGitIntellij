830a2c984449d761f77211de50f7fc7bd18fcf20
{Rajeshyadav111} <{rajeshmarri234@gmail.com}>
2023-03-20 19:52:03 +0530

    Initial Commit

---
+++ spring-mongo-embeded/src/main/java/com/javatechie/spring/mongo/embeded/api/controller/Amazon.java
@@ -0,0 +1,10 @@
+package com.javatechie.spring.mongo.embeded.api.controller;
+
+import ch.qos.logback.core.CoreConstants;
+
+public class Amazon {
+
+    public static void main(String[] args) {
+        System.out.println("Amazon");
+    }
+}
---
+++ spring-mongo-embeded/src/test/java/AmazonTest.java
@@ -0,0 +1,6 @@
+public class AmazonTest {
+
+    public static void main(String[] args) {
+        System.out.println("Amazon Test");
+    }
+}
